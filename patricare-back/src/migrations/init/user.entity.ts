bd19ad5b78705d5838a7267c9a90e0091d1a52d4
Constant Berthereau <constant.berthereau@gmail.com>
2025-11-25 15:58:50 +0100

    feat: tentative de migration

---
+++ patricare-back/src/migrations/init/user.entity.ts
@@ -0,0 +1,25 @@
+import { Entity, PrimaryGeneratedColumn, Column, ManyToOne, OneToMany } from "typeorm";
+import { Logement } from "./logement.entity";
+import { Role } from "./role.entity";
+import { Documents } from "./document.entity";
+
+@Entity()
+export class User {
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column({ length: 50 })
+    firstname: string;
+
+    @Column({ length: 50 })
+    lastname: string;
+
+    @ManyToOne(() => Role, role => role.users, { eager: true })
+    role: Role;
+
+    @OneToMany(() => Logement, logement => logement.owner)
+    logements: Logement[];
+
+    @OneToMany(() => Documents, doc => doc.user)
+    documents: Document[];
+}
