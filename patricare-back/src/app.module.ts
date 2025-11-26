a4cee60bd6445fbe0b2025cd0facc299c19a0d17
Constant Berthereau <constant.berthereau@gmail.com>
2025-11-26 16:29:00 +0100

    fix: import class

--- patricare-back/src/app.module.ts
+++ patricare-back/src/app.module.ts
@@ -1,10 +1,26 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from './modules/user/user.module';
+import { RoleModule } from './modules/role/role.module';
+import { DocumentModule } from './modules/document/document.module';
+import { AlertModule } from './modules/alert/alert.module';
+import { LodgingModule } from './modules/lodging/lodging.module';
+import { AddressModule } from './modules/address/address.module';
+import { CityModule } from './modules/city/city.module';
 
 @Module({
-  imports: [],
+  imports: [
+    UserModule,
+    RoleModule,
+    DocumentModule,
+    AlertModule,
+    LodgingModule,
+    AddressModule,
+    CityModule,
+
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
